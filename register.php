8dc0e9def6ea0efa59ca1e48666f49feeb61f6c8
ManuLdb <emmanuellorenzoldb@gmail.com>
2016-10-11 16:44:54 +0200

    ajout verif login existant

--- register.php
+++ register.php
@@ -2,20 +2,27 @@
 require('config/db.php');
 if (isset($_POST['pseudo']) && !empty($_POST['pseudo'])
     && isset($_POST['password']) && !empty($_POST['password'])
-){ /* stockage des données*/
-    $request = $db->prepare('INSERT INTO author (pseudo, password) VALUES(:pseudo,:password)');
-    $request->execute(
-        array(
-            'pseudo'=>$_POST['pseudo'],
-            'password'=>$_POST['password']
-        )
+    ) {
+    $reponse = $db->query('SELECT pseudo FROM author WHERE pseudo="'.$_POST['pseudo'].'"');
+    if ($donnees = $reponse->fetch()) {
+            echo 'erreur pseudo déjà enregistré';
+        } else {
+        /* stockage des données*/
+        $request = $db->prepare('INSERT INTO author (pseudo, password) VALUES(:pseudo,:password)');
+        $request->execute(
+            array(
+                'pseudo'=>$_POST['pseudo'],
+                'password'=>$_POST['password']
+            )
 
-    );
-    /*lien de la redirection*/
+        );
+        header('Location:login.php');
+    }
+        };
 
-};
+
 ?>
-<form action="login.php" method="post">
+<form action="register.php" method="post">
     <input type="text" name="pseudo" placeholder="Pseudo" required/>
     <input type="password" name="password" placeholder="Mot de passe" required/>
     <input type="submit" Value="S'inscrire"/>
